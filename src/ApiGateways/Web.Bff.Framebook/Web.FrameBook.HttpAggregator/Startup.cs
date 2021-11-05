a23a4ae220315422d289e557cad5d865a6f37846
Victor Aires Mendonça <victorairesm@gmail.com>
2021-11-05 12:03:43 -0300

    ajuste agregattor

--- src/ApiGateways/Web.Bff.Framebook/Web.FrameBook.HttpAggregator/Startup.cs
+++ src/ApiGateways/Web.Bff.Framebook/Web.FrameBook.HttpAggregator/Startup.cs
@@ -64,6 +64,12 @@ namespace Web.FrameBook.HttpAggregator
 
             app.UseOcelot().Wait();
 
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
+
         }
     }
 }
